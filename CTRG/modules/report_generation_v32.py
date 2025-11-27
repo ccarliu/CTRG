5b17e852c886897e7b67522df4a5c81d85cf4260
HongLiu <32379010+ccarliu@users.noreply.github.com>
2025-11-27 17:19:19 +0800

    Update report_generation_v32.py

--- CTRG/modules/report_generation_v32.py
+++ CTRG/modules/report_generation_v32.py
@@ -425,7 +425,7 @@ class M3AETransformerSS_3D_lmae_rg_v32(pl.LightningModule):
                 caption = self.tokenizer.decode(output, skip_special_tokens=True)
                 captions.append(caption[:])
         else:
-            captions = ["I am fxxking man."] # for test 
+            captions = ["take place for test."] # for test 
 
 
 
