0d6c7a31df89530c9bb033221e5244c7eecc7b4c
HongLiu <32379010+ccarliu@users.noreply.github.com>
2025-12-08 14:42:06 +0800

    Refactor imports and update data module initialization

--- pretrained_visual_feature_extract.py
+++ pretrained_visual_feature_extract.py
@@ -4,11 +4,9 @@ import resource
 
 import pytorch_lightning as pl
 
-from m3ae.config import ex
-from m3ae.datamodules.multitask_datamodule import MTDataModule
-from m3ae.datamodules.pretraining_medicat_datamodule import MedicatDataModule_3D, MedicatDataModule_3D_RATE, MedicatDataModule_3D_RATE_hr
-from m3ae.datasets.pretraining_ctrg_dataset import CTRGDataset
-from m3ae.modules import M3AETransformerSS_3D_lmae_rg_pretrain_v29
+from CTRG.config import ex
+from CTRG.datamodules.pretraining_medicat_datamodule import MedicatDataModule_3D_RATE_hr
+from CTRG.modules import M3AETransformerSS_3D_lmae_rg_pretrain_v29
 
 import torch
 import torch.nn as nn
@@ -37,15 +35,17 @@ def main(_config):
     # Data modules
 
     _config["num_workers"] = 8
-    dm = MedicatDataModule_3D_RATE_hr(_config)
+    
 
     # Module
     _config["image_size"] = [480, 480]
     _config["image_depth"] = 240
-    _config["batch_size"] = 1
+    _config["per_gpu_batchsize"] = 1
 
     _config["mask_rate"] = 0
 
+    dm = MedicatDataModule_3D_RATE_hr(_config)
+    
     dm.setup("test")
 
     model = M3AETransformerSS_3D_lmae_rg_pretrain_v29(_config)
@@ -62,9 +62,6 @@ def main(_config):
     model.load_state_dict(ckpt["state_dict"])
 
     model = model.cuda()
-
-    dm.setup("test")
-
 
 
     for idx, batch in enumerate(dm.test_dataloader()):
